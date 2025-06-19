8db153497de24ba94b49beaa44129371cfe97b6c
okl <chenlangl@outlook.com>
2025-06-19 20:06:05 +0800

    修改max98357驱动（已测试）

--- pet/main/driver/audio/hal_i2s.c
+++ pet/main/driver/audio/hal_i2s.c
@@ -11,29 +11,36 @@ esp_codec_dev_handle_t play_dev_handle;
 
 const audio_codec_data_if_t *hal_i2s_init(i2s_port_t i2s_port, hal_i2s_pin_t pin_config, i2s_chan_handle_t *tx_channel, uint16_t sample_rate)
 {
+    esp_err_t ret;
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(i2s_port, I2S_ROLE_MASTER);
     chan_cfg.auto_clear = true;
     ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, tx_channel, NULL));
 
-    i2s_tdm_config_t tdm_config = {
-        .clk_cfg = I2S_TDM_CLK_DEFAULT_CONFIG(sample_rate),
-        .slot_cfg = I2S_TDM_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO, I2S_TDM_SLOT0 | I2S_TDM_SLOT1 | I2S_TDM_SLOT2 | I2S_TDM_SLOT3),
+    i2s_std_config_t std_cfg = {
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(sample_rate),
+        .slot_cfg = {
+            .data_bit_width = I2S_DATA_BIT_WIDTH_16BIT, // MAX98357 支持 16/24/32bit，一般选 16bit
+            .slot_bit_width = I2S_SLOT_BIT_WIDTH_AUTO,
+            .slot_mode = I2S_SLOT_MODE_STEREO,
+            .slot_mask = I2S_STD_SLOT_LEFT | I2S_STD_SLOT_RIGHT,
+        },
         .gpio_cfg = {
-            .mclk = I2S_GPIO_UNUSED,
+            .mclk = I2S_GPIO_UNUSED, // MAX98357 不需要 MCLK
             .bclk = pin_config.bclk_pin,
-            .dout = pin_config.dout_pin,
             .ws = pin_config.ws_pin,
+            .dout = pin_config.dout_pin,
+            .din = I2S_GPIO_UNUSED,
             .invert_flags = {
-                .bclk_inv = false,
                 .mclk_inv = false,
+                .bclk_inv = false,
                 .ws_inv = false,
             },
         },
     };
 
-    tdm_config.clk_cfg.mclk_multiple = I2S_MCLK_MULTIPLE_512;
-    ESP_ERROR_CHECK(i2s_channel_init_tdm_mode(*tx_channel, &tdm_config));
-    ESP_ERROR_CHECK(i2s_channel_enable((*tx_channel)));
+    ret = i2s_channel_init_std_mode(*tx_channel, &std_cfg);
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(i2s_channel_enable(*tx_channel));
 
     audio_codec_i2s_cfg_t i2s_cfg = {
         .port = i2s_port,
@@ -72,7 +79,7 @@ static esp_err_t aduio_app_mute_function(AUDIO_PLAYER_MUTE_SETTING setting)
 esp_err_t aduio_app_write(void *audio_buffer, size_t len, size_t *bytes_written, uint32_t timeout_ms)
 {
     esp_err_t ret = ESP_OK;
-
+    ESP_LOGI("AUDIO_WRITE", "Write %d bytes", len);
     if (aduio_write(audio_buffer, len, bytes_written, 1000) != ESP_OK) {
         ret = ESP_FAIL;
     }
@@ -139,7 +146,7 @@ esp_err_t audio_app_player_init(i2s_port_t i2s_port, hal_i2s_pin_t pin_cfg, uint
         return ESP_FAIL;
     }
 
-    esp_codec_dev_set_out_vol(play_dev_handle, 50); // Set volume
+    esp_codec_dev_set_out_vol(play_dev_handle, 100); // Set volume
 
     audio_player_config_t config = {
         .mute_fn = aduio_app_mute_function,
